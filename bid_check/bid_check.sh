a09b4e45c5517b586c98fccd521486020755efa1
calr0x <cal>
2021-06-08 12:11:18 -0500

    .

--- bid_check/bid_check.sh
+++ bid_check/bid_check.sh
@@ -9,7 +9,8 @@
 BID_INTERVAL="15m"
 
 BIDS=$(docker logs --since $BID_INTERVAL otnode | grep Accepting | wc -l)
+echo $BIDS
 
 if [ $BIDS -eq 0 ]; then
-  /root/OT-NodeWatch/data/send.sh "Has not bid in the last $(BID_INTERVAL)"
+  /root/OT-NodeWatch/data/send.sh "Has not bid in the last $BID_INTERVAL"
 fi
