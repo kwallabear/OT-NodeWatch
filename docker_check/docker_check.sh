fdae8b22376365336e51449c20b88a5e925e35d1
calr0x <cal>
2021-06-10 17:56:06 -0500

    .

--- docker_check/docker_check.sh
+++ docker_check/docker_check.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
-# This script checks that the "otnode" docker container is running.
+# This script includes multiple checks:
 #
+# 1. Checks that the "otnode" docker container is running.
+# 2. Checks if the node has updated in the past 5 minutes
+
 
 RUNNING=$(docker ps | grep otnode | wc -l)
 echo $RUNNING
 
 if [ $RUNNING -eq 0 ]; then
   /root/OT-NodeWatch/data/send.sh "Node is NOT running!"
+fi
+
+UPDATED=$(docker logs --since 5m otnode | grep updated | wc -l)
+echo $UPDATED
+
+if [ $UPDATED -eq 1 ]; then
+  /root/OT-NodeWatch/data/send.sh "Node has updated to the newest version!"
 fi
