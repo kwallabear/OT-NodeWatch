a4c8b6518340056833b1b6ffe8c11b4f92b3ba86
calr0x <cal>
2021-06-10 07:00:10 -0500

    .

--- docker_check/docker_check.sh
+++ docker_check/docker_check.sh
@@ -3,10 +3,9 @@
 # This script checks that the "otnode" docker container is running.
 #
 
-BIDS=$(
-)
-echo $BIDS
+RUNNING=$(docker ps | grep otnode | wc -l)
+echo $RUNNING
 
-if [ $BIDS -eq 0 ]; then
+if [ $RUNNING -eq 0 ]; then
   /root/OT-NodeWatch/data/send.sh "Node is NOT running!"
 fi
